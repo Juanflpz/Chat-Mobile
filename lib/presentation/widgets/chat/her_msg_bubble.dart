7cad6b703f22b7ae71e6a5654e40bc6eeb89eab9
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-04-11 01:09:43 -0500

    feat: :sparkles: Loading alt of image message implemented

--- lib/presentation/widgets/chat/her_msg_bubble.dart
+++ lib/presentation/widgets/chat/her_msg_bubble.dart
@@ -38,9 +38,21 @@ class _ImageBubble extends StatelessWidget {
     return ClipRRect(
         borderRadius: BorderRadius.circular(20),
         child: Image.network(
-            "https://yesno.wtf/assets/no/23-5fe6c1ca6c78e7bf9a7cf43e406fb8db.gif",
-            width: size.width * 0.7,
-            height: 150,
-            fit: BoxFit.cover));
+          "https://yesno.wtf/assets/no/23-5fe6c1ca6c78e7bf9a7cf43e406fb8db.gif",
+          width: size.width * 0.7,
+          height: 150,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            //si ya no está cargando entonces muestra la imagen 
+            if (loadingProgress == null) return child; 
+
+            return Container(
+                width: size.width * 0.7,
+                height: 150,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: const Text("Mi amor está enviando una imagen"));
+          },
+        ));
   }
 }
